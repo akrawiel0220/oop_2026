55d0e191bbec50fba73434840419f36b1717a171
akrawiel0220 <akrawielinfo@gmail.com>
2026-01-03 10:08:20 +0000

    add new class modul

---
+++ klasy.py
@@ -0,0 +1,10 @@
+class Czlowiek:
+    gatunek = 'Homo sapiens'
+    def __init__(self): #to konstrukto ale to tez jest metoda
+        print("Niech powstanie Czlowiek")
+
+
+# powstawanie obiektu
+adam = Czlowiek()
+print(adam.gatunek)
+
