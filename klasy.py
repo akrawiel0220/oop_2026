9417da94561a51b72da81c9909513c3a381582fa
akrawiel0220 <akrawielinfo@gmail.com>
2026-01-03 10:28:46 +0000

    new content

--- klasy.py
+++ klasy.py
@@ -1,10 +1,17 @@
+# Klasa = Szablon, Przepis
 class Czlowiek:
-    gatunek = 'Homo sapiens'
-    def __init__(self): #to konstrukto ale to tez jest metoda
-        print("Niech powstanie Czlowiek")
+    # Istota
+    gatunek = "Homo Sapiens"
+    def __init__(self, imie):
+        # Konstruktor
+        # Akt Istnienia
+        print(f"Niech powstanie Czlowiek o imieniu {imie}")
+        self.imie = imie
 
-
-# powstawanie obiektu
-adam = Czlowiek()
+# Powstawanie obiektu
+# Gotowanie z przepisu
+adam = Czlowiek("Adam") # a = 4 # a = int(4)
+ewa= Czlowiek("Ewa") # a = 4 # a = int(4)
 print(adam.gatunek)
-
+print(ewa.gatunek)
+print(adam.imie)
